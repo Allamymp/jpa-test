f70ecd4357cb9bee818ec913a677cf1728bab7eb
allamymp <allamympereira@gmail.com>
2023-05-21 14:50:37 -0300

    updated

--- project/src/application/Program.java
+++ project/src/application/Program.java
@@ -1,7 +1,15 @@
 package application;
 
+import dominio.Pessoa;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
     }
 }
