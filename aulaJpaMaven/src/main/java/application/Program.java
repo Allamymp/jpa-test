90e2ecdffad83ce2820e1e760b7a98164805ead2
allamymp <allamympereira@gmail.com>
2023-05-22 06:28:39 -0300

    refatored

--- aulaJpaMaven/src/main/java/application/Program.java
+++ aulaJpaMaven/src/main/java/application/Program.java
@@ -1,4 +1,25 @@
 package application;
 
+import domain.Pessoa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Program {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Finished!");
+    }
 }
