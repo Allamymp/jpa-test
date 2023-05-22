d2bae748a3131ee2963a6945622bb9be49182eb9
allamymp <allamympereira@gmail.com>
2023-05-22 13:46:31 -0300

    addObject() implemented

--- aulaJpaMaven/src/main/java/application/Program.java
+++ aulaJpaMaven/src/main/java/application/Program.java
@@ -8,18 +8,23 @@ import jakarta.persistence.Persistence;
 
 public class Program {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
+        Pessoa pessoa = em.find(Pessoa.class,2);
+        System.out.println(pessoa);
+        //addObject(em);
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+       em.close();
+       emf.close();
         System.out.println("Finished!");
+    }
+
+    public static void addObject(EntityManager em){
+        Pessoa test = new Pessoa(null,"test name","email@test.com");
+        em.getTransaction().begin();
+         em.persist(test);
+        em.getTransaction().commit();
+        System.out.println("add finished!");
+
     }
 }
